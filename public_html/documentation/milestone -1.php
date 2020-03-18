49e9a5e78c6b43b18d43448e68ac72d5ff5ab41b
India Byrne <byrneindia@gmail.com>
2020-03-18 14:45:40 -0600

    First push.

--- public_html/documentation/milestone -1.php
+++ public_html/documentation/milestone -1.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="./styles.css">
+	<title>Data Design Project</title>
+</head>
+<body>
+<li><strong>Purpose:</strong></li>
+<br>To showcase my valuable skills in order to get an internship.
