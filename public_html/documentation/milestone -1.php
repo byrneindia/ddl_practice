a8c632231abd272d5273832a81c9c51a3e5a7ee5
India Byrne <byrneindia@gmail.com>
2020-03-31 18:34:03 -0600

    Testing fix.

--- public_html/documentation/milestone -1.php
+++ public_html/documentation/milestone -1.php
@@ -2,9 +2,23 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="./styles.css">
-	<title>Data Design Project</title>
+	<title>Milestone-1</title>
 </head>
 <body>
-<li><strong>Purpose:</strong></li>
-<br>To showcase my valuable skills in order to get an internship.
+<li><strong>Purpose:</strong></li>To showcase my valuable skills in order to get an internship.
+<li><strong>Audience:</strong></li>Potential employers, instructors, and clients.
+<li><strong>Goal</strong></li>
+To acquire a full-time internship position.
+<br>To increase my current skill set and hourly worth.
+<br>To create a personal portfolio.
+<br>To learn SOPs of a larger company.
+<br>To expand my network.
+<li><strong>Persona:</strong>
+	<br>India Byrne
+	<br><strong>Age:</strong> 26
+			<br><strong>Profession:</strong> Web Development Full Stack Alumni
+			<br><strong>Technology:</strong>
+			<br>iOS, Windows 10, Andriod Phone, Home Wifi
+			<br><strong>Attitudes and Behaviors:</strong>
+			<br>
+
