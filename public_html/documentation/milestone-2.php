b5f7dadeb6a3f8627b6c68d8f4a58cad0f52b2bb
India Byrne <byrneindia@gmail.com>
2020-04-08 15:10:04 -0600

    Adding new content information.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,10 +7,12 @@
 <body>
 <li><strong>Welcome:</strong></li>
 I want to have the front page to be memorable I searched some personal website galleries and the following links are my choices.
-I will need to see if these kinds of animation are within my skill set but Steve Losh is my favorite so far.
+I will need to see if these kinds of animation are within my skill set but "Steve Losh" is my favorite so far.
 1.<a href="https://stevelosh.com/"></a>
 2.<a href="http://www.rleonardi.com/interactive-resume/"></a>
 3.<a href="https://www.adamhartwig.co.uk/"></a>
+As far as wording I would like my contact information and name to be the first thing seen.
+It is very unquie and that will stick in the mind of my veiwer.
 
 <li><strong>About Me:</strong></li>.
 <br>India Byrne
