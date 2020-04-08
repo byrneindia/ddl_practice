374707ba08ac7813962d412789085dd5fe1ec29b
India Byrne <byrneindia@gmail.com>
2020-04-08 15:46:26 -0600

    Adding new content information.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -13,13 +13,17 @@ I will need to see if these kinds of animation are within my skill set but "Stev
 3.<a href="https://www.adamhartwig.co.uk/"></a>
 As far as wording I would like my contact information and name to be the first thing seen.
 It is very unquie and that will stick in the mind of my veiwer.
-Contact links to Linkedin, Instagram, and Email need to be featured.
-Colors  will have a bright palette.
+Colors  will have a bright palette and I want to at least animate my image somewhere, possible potrait?
 <li><strong>About Me:</strong></li>.
 <br>India Byrne
 <br><strong>Age:</strong> 26
 <br><strong>Profession:</strong> Web Development Full Stack Alumni
 <br><strong>Technology:</strong>
 <br>iOS, Windows 10, Andriod Phone, Home Wifi
+<br><strong>Experience:</strong><br>
+<br>I want to show case all of mu current skills as well as my previous works skills as well. I will only be featuring:
+<br> Canon, Veritude, Lavu, and Unity BPO.
 <li><strong>Portfolio:</strong></li>
+I will need help with creating this, I don't know which of my assignments besides Capstone and the website itself.
 <li><strong>Contact:</strong></li>
+Contact links to Linkedin, Instagram, and Email need to be featured. No Facebook
