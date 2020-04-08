88ebab8ec26204f6715dd2fd707bb9aaaebd1491
India Byrne <byrneindia@gmail.com>
2020-04-08 15:02:12 -0600

    Adding new content information.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,3 +5,18 @@
 	<title>Milestone-1</title>
 </head>
 <body>
+<li><strong>Welcome:</strong></li>
+I want to have the front page to be memorable I searched some personal website galleries and the following links are my choices.
+I will need to see if these kinds of animation are within my skill set but Steve Losh is my favorite so far.
+1.<a href="https://stevelosh.com/"></a>
+2.<a href="http://www.rleonardi.com/interactive-resume/"></a>
+3.<a href="https://www.adamhartwig.co.uk/"></a>
+
+<li><strong>About Me:</strong></li>.
+<br>India Byrne
+<br><strong>Age:</strong> 26
+<br><strong>Profession:</strong> Web Development Full Stack Alumni
+<br><strong>Technology:</strong>
+<br>iOS, Windows 10, Andriod Phone, Home Wifi
+<li><strong>Portfolio:</strong></li>
+<li><strong>Contact:</strong></li>
